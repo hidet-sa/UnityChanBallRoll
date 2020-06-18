3f8c65f86039a6223323e842a1a01b06312c0b5a
Sakihama Hidetoshi <sakihama-h@kadokawa.jp>
2020-06-18 19:25:36 +0900

    初回コミット

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed = 10;
+    int count;
+    public Text countText;
+    AudioSource getSE;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Debug.Log("Hallo,World!");
+        getSE = GetComponent<AudioSource>();
+        count = 0;
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        
+        Vector3 move = new Vector3(moveH, 0, moveV);
+        rb.AddForce(move * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Item"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            Debug.Log(count);
+            SetCountText();
+            getSE.Play();
+        }
+        else if(other.gameObject.CompareTag("StageBottom")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "ゲット数：" + count.ToString();
+    }
+}
